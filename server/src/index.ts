b851d6ab6073ae1c33ceb79b8034f9cae9e57626
HITESH BALAJI VS <hiteshbalaji685@gmail.com>
2026-03-01 23:29:06 +0530

    Fix start script for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,6 +18,8 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
